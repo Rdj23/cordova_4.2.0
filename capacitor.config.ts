1db66a5bd178be0147a08e7ce4d2eacd1499de42
Rdj23 <rohan.jadhav@clevertap.com>
2025-10-01 05:17:02 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ct-demo-ionic',
+  webDir: 'www'
+};
+
+export default config;
